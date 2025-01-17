e678dd10609f0b00d083bc5d27f212e8fde63311
Kyle <kyle.lee@factblock.com>
2025-01-17 10:52:23 +0900

    delete

--- src/api/home/index.ts
+++ src/api/home/index.ts
@@ -4,16 +4,15 @@ import { BannerPrams, CommunityCardResponse } from "@dto/bannerprams";
 import { BannerResponse } from "@dto/BannerResponse";
 import { headers } from "next/headers";
 
-export const getBanners = async (params: BannerPrams) => {
-    const { data } = await axios.get<BannerResponse>(`https://cat-fact.herokuapp.com/facts/`);
+export const getBanners = async () => {
+    const { data } = await axios.get(`https://cat-fact.herokuapp.com/facts/`);
     return data;
   };
 
 
   export const getCommunitiesForHome = async () => {
-    const { data } = await axios.get<CommunityCardResponse>(
+    const { data } = await axios.get(
       `https://dog.ceo/api/breeds/list/all`, 
     );
-  
     return data;
   };
